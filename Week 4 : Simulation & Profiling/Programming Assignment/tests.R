ddb47065717ef5767c75304d7c571ce97434092e
josousa82 <j.alves.sousa82@gmail.com>
2018-01-25 20:12:36 +0000

    Updated tests

--- Week 4 : Simulation & Profiling/Programming Assignment/tests.R
+++ Week 4 : Simulation & Profiling/Programming Assignment/tests.R
@@ -11,5 +11,37 @@ longerData <- cleanOutcome %>% gather(Mortality/Readmission, Rates, 5:10 )
 
 longerData <- longerData[ longerData == "Not Available" ] <- NA
 
-colnames(cleanOutcome) <- c(".Hospital", ".City",  ".St", ".County", ".Mortality.H.Attack", ".Mortality.H.Failure", ".Mortality.Pneumonia", ".Read.H.Attack", ".Read.H.Failure", ".Read.Pneumonia" )
-head(cleanOutcome)
+colnames(cleanOutcome) <- c(".Hospital", ".City",  ".St", ".County", ".Mortality.H.Attack", 
+                            ".Mortality.H.Failure", ".Mortality.Pneumonia", ".Read.H.Attack", 
+                            ".Read.H.Failure", ".Read.Pneumonia" )
+
+
+
+
+## getting NA's
+
+transform(hospitaData, Provider.Number = as.numeric(Provider.Number), ZIP.Code = as.numeric(ZIP.Code), 
+          Phone.Number = as.numeric(Phone.Number))
+
+cleanOutcome <- transform(cleanOutcome, .Mortality.H.Attack = as.numeric(.Mortality.H.Attack),
+                          .Mortality.H.Failure = as.numeric(.Mortality.H.Failure), .Mortality.Pneumonia = as.numeric(.Mortality.Pneumonia))
+
+
+cleanOutcome <- transform(cleanOutcome, .Read.H.Attack = as.numeric(.Read.H.Attack),
+                          .Read.H.Failure = as.numeric(.Read.H.Failure), .Read.Pneumonia = as.numeric(.Read.Pneumonia))
+
+## organizing the results and removing NA's with dplyr
+
+cleanOutcome %>%
+    na.omit()%>%
+    select(.Hospital, .St, .Mortality.H.Attack)%>%
+    arrange(desc(.Mortality.H.Attack))%>%
+    head()
+
+cleanOutcome %>%
+    na.omit()%>%
+    group_by(.Hospital, .St) %>%
+    arrange(asc(.Mortality.H.Attack))%>%
+    des
+
+
