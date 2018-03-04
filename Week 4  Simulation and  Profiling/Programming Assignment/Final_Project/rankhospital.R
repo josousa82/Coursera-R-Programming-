ed1f745b8aea16b93143eae0b1fc421b887621b5
josousa82 <j.alves.sousa82@gmail.com>
2018-03-04 15:42:37 +0000

    final

---
+++ Week 4  Simulation and  Profiling/Programming Assignment/Final_Project/rankhospital.R
@@ -0,0 +1,57 @@
+require(dplyr)
+require(rlang)
+
+lsource <- c("cachedf.R", "readDataset.R", "data.clean.R")
+lapply(lsource, source)
+
+rankhospital <- function(state, outcome, num = as.numeric()) {
+    ## Read outcome data
+    ## Check that state and outcome are valid
+    ## Return hospital name in that state with the given rank
+    ## 30-day death rate
+    
+    df.final <- clean("Data", "outcome-of-care-measures.csv")
+    
+    
+    cond <- levels(ch.df$get.df()[, 3])
+    state.check <- levels(ch.df$get.df()[, 2])
+
+    tryCatch(
+        {
+            match.arg(outcome, cond, several.ok = TRUE)
+        },
+        error = function(e) {
+            return(message(sprintf("Error in best(\"%s\", \"%s\") : invalid outcome", state, outcome)))
+            stop(e)
+        },
+        finally = function(){
+            stop("STOP")
+        }
+    )
+    tryCatch(
+        {
+            match.arg(state, state.check, several.ok = TRUE)
+        },
+        error = function(e){
+            return(message(sprintf("Error in best(\"%s\",\"%s\") : invalid state", state, outcome)))
+        },
+        finally = function(){
+            stop(e)
+        }
+        
+    )
+    
+    df.t <- df.final%>% dplyr::filter(df.final[[".St"]] == state 
+                                      & df.final[["condition"]] == outcome, 
+                                      !is.na(df.final[["rate"]]))
+
+    df.tt <- cbind(df.t[order(df.t$rate, df.t$.Hospital), ], rank = c(1:length(df.t$rate)))
+    
+    
+    print(subset(df.tt$.Hospital, df.tt$rank == num ))
+    print(head(df.tt, 5))
+    print(summary(df.tt))
+    
+    
+    
+}
