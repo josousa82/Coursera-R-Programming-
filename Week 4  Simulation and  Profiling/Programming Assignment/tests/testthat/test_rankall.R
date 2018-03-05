6addf6d5ea1c759f0b331f970ae1e8f8b338aea4
josousa82 <j.alves.sousa82@gmail.com>
2018-03-05 01:41:39 +0000

    added test rankall

---
+++ Week 4  Simulation and  Profiling/Programming Assignment/tests/testthat/test_rankall.R
@@ -0,0 +1,9 @@
+# Test Rank All function
+
+context("Test that Function rankall returns a dataframe, with the desirable results")
+
+testthat("That the Rankall function returns a dataframe", {
+    
+    
+    
+})
