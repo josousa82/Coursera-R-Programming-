1a898c6b285cf5993d62f7f317d8987903e68b6c
josousa82 <j.alves.sousa82@gmail.com>
2018-03-05 03:19:49 +0000

    package

--- Week 4  Simulation and  Profiling/Programming Assignment/Final_Project/ProAssingnment3/R/cachedf.R
+++ Week 4  Simulation and  Profiling/Programming Assignment/Final_Project/ProAssingnment3/R/cachedf.R
@@ -1,3 +1,20 @@
+#' Makes the cache of the dataframe, in Raw and cleaned.
+#'
+#' @param x Data Frame to be stored 
+#' @param .set Set the Raw Data in cache
+#' @param .get Get the Raw Data stored in Data Frame in cache
+#' @param .set.df Set the cleaned Data stored in a Data Frame in cache
+#' @param .get.df Get the cleaned Data stored in a Data Frame in cache
+#' @return a DataFrame in Raw or cleaned data
+#'   
+#' @seealso \code{\link{nchar}} which this function wraps
+#' @export
+#' @examples
+#'  d <- data.frame
+#'  cached.df <- 
+#'  
+#'  
+#'  
 cache.df <- function(x = as.data.frame()){
     
     clean.df <- NULL
--- Week 4  Simulation and  Profiling/Programming Assignment/Final_Project/ProAssingnment3/R/readDataset.R
+++ Week 4  Simulation and  Profiling/Programming Assignment/Final_Project/ProAssingnment3/R/readDataset.R
@@ -1,3 +1,14 @@
+#' Reads Dataset (in characters).
+#'
+#' @param directory Folder for where the datafile. Default Value "Data"
+#' @param file Name of the file to be read
+#' @return a DataFrame with the Raw data
+#'   
+#' @seealso \code{\link{nchar}} which this function wraps
+#' @export
+#' @examples
+#' read.dataset("Folder", "file")
+
 read.dataset <- function(directory = c("Data"), file){
     
     path.to.file <- file.path(directory, file)
